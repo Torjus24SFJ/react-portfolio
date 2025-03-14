6c1706fa0d2340f1709b8e2b1aed59d102df154d
Torjus <Torjus24sfj@gmail.com>
2025-03-14 14:12:58 +0100

    fixed project img

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,21 +4,26 @@ export function Projects() {
   return (
     <>
       <section id="projects" className="flex flex-col mr-30">
-         <Heading alignment="right">
-                  <h2>
-                    Projects
-                  </h2>
-                </Heading>
-        <div className="grid grid-cols-4 mt-20 gap-5"> 
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            <span className="w-50 h-50 bg-white"/>
-            
+        <Heading alignment="right">
+          <h2>Projects</h2>
+        </Heading>
+        <div className="grid grid-cols-4 mt-20 gap-5">
+          <a href="https://e-commerce-kappa-pearl.vercel.app/">
+            <div className="w-50 h-50 cursor-pointer">
+              <img
+                src="/public/assets/webshop.png"
+                className="w-cover h-cover"
+                alt="Webstore"
+              />
+            </div>
+          </a>
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
+          <span className="w-50 h-50" />
         </div>
       </section>
     </>
